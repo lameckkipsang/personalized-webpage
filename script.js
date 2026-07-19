9c6dbf6c25a69ef5ea075fa26bbf4b90a3236205
Lameck Kipsang <lameck.ky@gmail.com>
2026-07-19 19:11:34 +0300

    add saveData function and the months calculation function

---
+++ script.js
@@ -0,0 +1,8 @@
+function calculateMonths(years) {
+    let months = years * 12;
+    return months;
+}
+function saveData() {
+    let userName = document.getElementById("name").value;
+    let userAge = document.getElementById("age").value;
+}
