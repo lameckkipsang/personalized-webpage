91fd4d255348db72c938a61bc753ff149cf453a6
Lameck Kipsang <lameck.ky@gmail.com>
2026-07-19 19:32:02 +0300

    add showContent function that allows for local storage and hides the hero section and heading 1 and paragraph

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ function saveData() {
     let userName = document.getElementById("name").value;
     let userAge = document.getElementById("age").value;
 }
+function showContent(){
+    let name = localStorage.getItem("savedName");
+    let age = localStorage.getItem("savedAge");
+    if (name == null || age == null) {
+        return; 
+    }   
+    document.getElementById("hero-section").style.display = "none";
+    document.getElementById("main-title").style.display = "none";
+    document.getElementById("main-desc").style.display = "none";
+    document.getElementById("output-section").style.display = "block";
+    document.getElementById("greeting").innerHTML = `Hello, ${name}!`;    
+}
